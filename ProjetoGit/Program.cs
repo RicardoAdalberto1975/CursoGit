088820aaed1c6485262645e147bbf261af36e7fb
Ricardo Adalberto Pereira <ricardoadalberto@gmail.com>
2020-04-23 14:11:08 -0300

    Segundo commit

--- ProjetoGit/Program.cs
+++ ProjetoGit/Program.cs
@@ -3,7 +3,7 @@
 namespace ProjetoGit {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá mundo!");
         }
     }
 }
