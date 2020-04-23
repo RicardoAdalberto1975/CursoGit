5edc4731fe4edc179a04d3b925e51b44faa99102
RicardoAdalberto1975 <ricardoadalberto@gmail.com>
2020-04-23 14:55:08 -0300

    Terceiro commit

--- ProjetoGit/Program.cs
+++ ProjetoGit/Program.cs
@@ -4,6 +4,7 @@ namespace ProjetoGit {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Teste Push");
         }
     }
 }
